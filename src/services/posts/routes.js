188a6fe6a2c330b663a6ce173948d5593f4c83b7
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-09-06 13:55:35 +0100

    Post Like route

--- src/services/posts/routes.js
+++ src/services/posts/routes.js
@@ -82,4 +82,26 @@ PostsRouter.put("/:id/image", multer({ storage: mediaStorage }).single("image"),
       next(error)
   }
 })
+
+PostsRouter.get("/:postId/like/:userId", async(req,res,next) => {
+  try {
+    const {postId, userId} = req.params
+    const isLiked = await Posts.find( { likes: userId } )
+    let response
+    if (isLiked.length === 0 ){
+       response = await Posts.findByIdAndUpdate(postId, {$push : {
+        likes: userId
+      }}, {new: true})
+      
+    } else {
+      response = await Posts.findByIdAndUpdate(postId, {$pull : {
+        likes: userId
+      }}, {new: true})
+    }
+    res.send({totalLikes: response.likes.length, currentUserLike: response.likes.includes(userId)? true: false})
+  } catch (error) {
+    res.status(500)
+    next(error)
+  }
+})
 export default PostsRouter;
