110a0e889328102a9d406f88a2509a0e70cb26c5
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-09-06 11:40:57 +0100

    Update routes.js

---
+++ src/services/posts/routes.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import createError from 'http-errors'
+import Posts from './schema.js'
+
+const PostsRouter = express.Router()
+
+PostsRouter.get("/", async(req,res,next) => {
+    try {
+        const allPost = await Posts.find({})
+        res.send(allPost)
+    } catch (error) {
+      next(error)
+    }
+})
+PostsRouter.get("/:_id", async(req,res,next) => {
+    try {
+      
+    } catch (error) {
+      next(error)
+    }
+})
+
+PostsRouter.post("/", async(req,res,next) => {
+    try {
+      
+    } catch (error) {
+      next(error)
+    }
+})
+PostsRouter.put("/:_id", async(req,res,next) => {
+    try {
+      
+    } catch (error) {
+      next(error)
+    }
+})
+PostsRouter.delete("/:_id", async(req,res,next) => {
+    try {
+      
+    } catch (error) {
+      next(error)
+    }
+})
+export default PostsRouter;
