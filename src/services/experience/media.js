f265b40ad378e5cbe4e1ee5398a274736178446e
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-09-06 12:50:05 +0100

    use one media save image to cloud

--- src/services/experience/media.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary'
-import { CloudinaryStorage } from 'multer-storage-cloudinary'
-
-export const mediaStorage = new CloudinaryStorage({
-    cloudinary,
-    params: {
-        folder: "LinkedIn Images"
-    }
-})
