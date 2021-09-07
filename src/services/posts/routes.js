e09fdc67aed342ec10a21866e5803bc0b5bbf5d5
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-09-07 11:07:00 +0100

    Post Route Comments CRUD

--- src/services/posts/routes.js
+++ src/services/posts/routes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import createError from 'http-errors'
 import Posts from './schema.js'
+import Comment from '../comments/schema.js'
 import multer from 'multer'
 import { mediaStorage } from '../../tools/saveImageCloudinary.js'
 
@@ -8,7 +9,7 @@ const PostsRouter = express.Router()
 
 PostsRouter.get("/", async(req,res,next) => {
     try {
-      const posts=await Posts.find({}).populate('user')
+      const posts=await Posts.find({}).populate('user').populate('Comments')
       res.send(posts)
     } catch (error) {
       next(error)
@@ -17,7 +18,7 @@ PostsRouter.get("/", async(req,res,next) => {
 PostsRouter.get("/:_id", async(req,res,next) => {
     try {
       const postId=req.params._id
-      const post=await Posts.findById(postId).populate('user')
+      const post=await Posts.findById(postId).populate('user').populate('Comments')
       if(post){
         res.send(post)
       }
@@ -104,4 +105,116 @@ PostsRouter.get("/:postId/like/:userId", async(req,res,next) => {
     next(error)
   }
 })
+
+PostsRouter.get("/:postId/comment", async (req, res, next) => {
+  try {
+    const post = await Posts.findById(req.params.postId).populate('Comments')
+    res.send(post.Comments)
+  } catch (error) {
+    next(error)
+  }
+})
+PostsRouter.get("/:postId/comment/:_id", async (req, res, next) => {
+  try {
+    const commentId = req.params._id
+    const comment = await Comment.findById(commentId)
+    if (comment) {
+      res.send(comment)
+    } else {
+      next(
+        createError(
+          404,
+          `The comment with the id ${req.params._id} was not not found!`
+        )
+      )
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+PostsRouter.post("/:postId/comment", async (req, res, next) => {
+  try {
+    const newComment = await Comment.create(req.body);
+    if (newComment) {
+      const commentId = newComment._id
+      const addComment = await Posts.findByIdAndUpdate(
+        req.params.postId,
+        { $push: { Comments: commentId } }
+      );
+      res.status(201).send(addComment);  
+    } else {
+      next(createHttpError(400, { errorsList }));
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+
+PostsRouter.put("/:postId/comment/:_id", async (req, res, next) => {
+  try {
+    const modifiedComment = await Comment.findByIdAndUpdate(
+      req.params._id,
+      req.body,
+      { new: true }
+    )
+    if (modifiedComment) {
+      res.send(modifiedComment)
+    } else {
+      next(
+        createError(
+          404,
+          `The comment with the id ${req.params._id} was nonot found`
+        )
+      )
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+PostsRouter.delete("/:postId/comment/:_id", async (req, res, next) => {
+  try {
+    const { _id,postId } = req.params
+    const deletedComment = await Comment.findByIdAndDelete(_id)
+    if (deletedComment) {
+      const DeleteCommentFromPost = await Posts.findByIdAndUpdate(
+        postId,
+        { $pull: { Comments: _id } }
+      );
+      res.status(200).send(`The comment with the id ${_id} was successfully deleted `)
+    } else {
+      next(createError(404, `The comment with the id ${_id} was not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+PostsRouter.put(
+  "/:id/image",
+  multer({ storage: mediaStorage }).single("image"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const data = await Comment.findById(id)
+      if (data) {
+        const modifiedComment = await Comment.findByIdAndUpdate(
+          id,
+          { image: req.file.path },
+          {
+            new: true,
+          }
+        )
+        res.send(modifiedComment)
+      } else {
+        next(createError(404, `The comment with the id ${id} was not found!`))
+      }
+    } catch (error) {
+      console.log(error)
+      next(error)
+    }
+  }
+)
 export default PostsRouter;
